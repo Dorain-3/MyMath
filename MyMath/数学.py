b2fb67d5f21d78ab959265e90c20ca69c7f6305c
Winnoooo <1450298356@qq.com>
2024-01-03 22:15:25 +0800

    sbhc

---
+++ MyMath/数学.py
@@ -0,0 +1,25 @@
+# 计算阶乘
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+# 计算余弦值
+def shushu_cosine(degrees, terms=10):
+    radians = degrees * (3.141592653589793 / 180)  # 角度转弧度
+    result = 0
+    for n in range(terms):
+        term = ((-1) ** n) * (radians ** (2 * n)) / factorial(2 * n)
+        result += term
+    return result
+
+def shushu_sine(degrees, terms=10):
+    radians = degrees * (3.141592653589793 / 180)
+    result = 0
+    for n in range(terms):
+        term = ((-1) ** n) * (radians ** (2 * n + 1)) / factorial(2 * n + 1)
+        result += term
+    return result
+
+print(shushu_cosine(60))
+import math
