e34e9f203c1c4920680481a18b506e373827e24b
name <3174950356@qq.com>
2024-01-04 03:09:09 +0800

    1_4

---
+++ MyMath/Java/MyMathMain.java
@@ -0,0 +1,73 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+public class MyMathMain {
+	public static void main(String[] args) {
+		Thread sinThread = new Thread(() -> {
+			double x = 0.0000000000000000;
+			try {
+				String fileName = "sin.txt";
+				FileWriter fileWriter = new FileWriter(fileName);
+				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+				DecimalFormat decimalFormat = new DecimalFormat("#.######");
+
+				while (x <= Math.PI) {
+					bufferedWriter.write(decimalFormat.format(x) + " " + decimalFormat.format(StrictMath.sin(x)));
+					bufferedWriter.newLine();
+					x += 0.000005;
+				}
+				bufferedWriter.close();
+				fileWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+		Thread cosThread = new Thread(() -> {
+			double x = 0.0000000000000000;
+			try {
+				String fileName = "cos.txt";
+				FileWriter fileWriter = new FileWriter(fileName);
+				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+				DecimalFormat decimalFormat = new DecimalFormat("#.######");
+
+				while (x <= Math.PI) {
+					bufferedWriter.write(decimalFormat.format(x) + " " + decimalFormat.format(StrictMath.cos(x)));
+					bufferedWriter.newLine();
+					x += 0.000005;
+				}
+				bufferedWriter.close();
+				fileWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+		Thread tanThread = new Thread(() -> {
+			double x = 0.0000000000000000;
+			try {
+				String fileName = "tan.txt";
+				FileWriter fileWriter = new FileWriter(fileName);
+				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+				DecimalFormat decimalFormat = new DecimalFormat("#.######");
+
+				while (x <= Math.PI) {
+					bufferedWriter.write(decimalFormat.format(x) + " " + decimalFormat.format(StrictMath.tan(x)));
+					bufferedWriter.newLine();
+					x += 0.000005;
+				}
+				bufferedWriter.close();
+				fileWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+
+		sinThread.start();
+		cosThread.start();
+		tanThread.start();
+	}
+}
---
+++ MyMath/Java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+	public static void main(String[] args) {
+		test();
+	}
+
+	public static void test() {
+
+		System.out.println(Math.sin(0.0000395));
+		System.out.println("0.00003949999999");
+	}
+
+}
